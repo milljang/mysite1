55c29d9fccc8040fb6124afad9192f84b40cdc4d
newzen <erp2001@naver.com>
2018-02-04 13:29:05 +0900

    제너릭뷰

--- polls/views.py
+++ polls/views.py
@@ -3,23 +3,26 @@ from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice , Question
 
-def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list}
-    return render(request , 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-# Create your views here.
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html' , {'question': question})
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request,question_id):
     question = get_object_or_404 (Question, pk=question_id)
